93393f6c3a27538699db814a6a5290b47bc3bb8f
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-30 21:54:31 +0530

    Added the meaningful comments for the script file...

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// these are the constants containing all important HTML elements 
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const container = document.querySelector(".container");
 
+// These are the event listeners for dynamically adding and removing show-nav class...
 open.addEventListener("click", () => {
   container.classList.add("show-nav");
 });
